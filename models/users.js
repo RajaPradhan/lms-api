5c0c10159627f3f73cd2f106a786467e33908b7c
Raja Pradhan <rajapradhan08@gmail.com>
2017-08-06 19:34:06 +0530

    Use sha512 to hash password

--- models/users.js
+++ models/users.js
@@ -32,11 +32,11 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, 'sha512').toString('hex');
 };
 
 userSchema.methods.isValidPassword = function(password) {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  const hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, 'sha512').toString('hex');
   return hash === this.hash;
 };
 
